0744ecfa2dde22edc4723f9c9f032fb9d8838adf
Ankit Gangrade <ankitgangrade9617@gmail.com>
2025-03-09 15:33:51 +0530

    api change

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use( 
   cors({
-    origin: "https://chating-with-anyone.vercel.app", 
+    origin: "https://chating-with-anyone.vercel.app/", 
     credentials: true,
   }) 
 );
