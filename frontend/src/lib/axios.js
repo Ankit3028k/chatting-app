1334ef039b1a83c04c432a5a4c6bac20e9d6fcf5
Ankit Gangrade <ankitgangrade9617@gmail.com>
2025-03-09 14:58:26 +0530

    api change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chating-app-eujl.onrender.com/api" : "https://chating-app-eujl.onrender.com/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chating-app-eujl.onrender.com/api" : "/api",
   withCredentials: true,
 });
